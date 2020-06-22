f635c71efd8e5778a141447dfae92f7860e833ac
libelink <berton.agnes@gmail.com>
2020-06-22 19:08:21 +0200

    Creating Posts

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', function () {
     return view('home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/login', 'LoginController@index')->name('login');
+
+Route::get('/home', 'PostsController@show')->name('Posts');
