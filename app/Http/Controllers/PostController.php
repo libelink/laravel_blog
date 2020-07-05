bc9f6f654c6116f1f30bd4903a0ce8e9002ec798
libelink <berton.agnes@gmail.com>
2020-07-05 16:45:42 +0200

    Posts CRUD, start building front-end

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
+use App\articles;
+use App\Comment;
 
 class PostController extends Controller
 {
-    public function show($post){
+    public function show($id){
+        // Si on utilise eloquent (1), pas de problème qd post seul eloquent resout directement si (2) erreur
+        $post = Articles::findorfail($id);
+        $comments = Comment::where('article_id', $id)->get();
 
-        $posts = DB::table('articles')->where('id', $post)->get();
+        return view('post')
+            ->with(compact('post'))
+            ->with(compact('comments'));
+    }
 
-        return view('home',[
-            'posts' => $posts
-        ]);
+    public function create(){
+        return view ('createpost');
+    }
 
+    public function edit($id){
+        $article = Articles::findorfail($id);
+        return view('editpost',compact('article'));
+    }
+
+    public function delete($id){
+        $post = Articles::findorfail($id);
+        $comments = Comment::where('article_id', $id)->get();
+        return view('deletepost')
+            ->with (compact('post'))
+            ->with(compact('comments'));
+    }
+
+
+
+    public function store(){
+        //dump(request()->all());
+        // possible car on a créé le modèle "articles"
+
+        $articles = new Articles();
+        $articles->title= request('title');
+        $articles->content=request('content');
+        $articles->save();
+
+        return redirect('/home');
+
+    }
+
+    public function update($id){
+        $article = Articles::findorfail($id);
+        $article->title=request('title');
+        $article->content=request('content');
+        $article->save();
+
+        return redirect('/home');
+
+
+    }
+
+    public function destroy($id){
+        $comments = Comment::where('article_id',$id)->delete();
+        $article = Articles::find($id);
+        $article->delete();
+
+        return redirect('/home');
     }
 }
